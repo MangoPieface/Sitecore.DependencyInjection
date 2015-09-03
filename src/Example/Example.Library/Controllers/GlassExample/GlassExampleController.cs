be3988bd895149611d3c79c193b0e76a0194e6fe
adamsimsy <adamsimsy@gmail.com>
2015-09-03 21:21:05 +0100

    Added working example of Glass Controller and View that is editable

---
+++ src/Example/Example.Library/Controllers/GlassExample/GlassExampleController.cs
@@ -0,0 +1,24 @@
+﻿using Example.Library.Models.GlassModels;
+using Example.Library.Services;
+using Glass.Mapper.Sc;
+using System.Web.Mvc;
+
+namespace Example.Library.Controllers.InjectionExample
+{
+    public class GlassExampleController : Controller
+    {
+        private ISitecoreContext _context;
+
+        public GlassExampleController()
+        {
+            _context = new SitecoreContext();
+        }
+
+        public ActionResult Index()
+        {
+            var item = _context.GetCurrentItem<Page>();
+
+            return View("~/Views/GlassExample/Index.cshtml", item);
+        }
+    }
+}
